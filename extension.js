872af8f0712f669df639763961e9b8fc139484d9
DeStInE-kay <destinykevogo@gmail.com>
2022-12-29 12:02:25 +0300

    code refactored

--- extension.js
+++ extension.js
@@ -9,28 +9,29 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("Leads"))
 deleteBtn.addEventListener("dblclick", function () {
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-button.addEventListener("click", clicked = ()=>{
-    myLeads.push(inputEl.value) 
-    renderLeads()
-    inputEl.value = ""
-    localStorage.setItem("Leads", JSON.stringify(myLeads))
-})
-function renderLeads(){
+function render(leads){
     let listItems = ""
-    for(i = 0; i < myLeads.length; i++){
-        // listItems += "<li><a href='"+ myLeads + "' target='_blank'>" + myLeads[i] + "</a></li>"
+    for(i = 0; i < leads.length; i++){
         listItems += `  <li>
-                        <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+                        <a href='${leads[i]}' target='_blank'>${leads[i]}</a>
                         </li>`
     }
 
     ulEl.innerHTML = listItems 
 }
+
+
+button.addEventListener("click", clicked = ()=>{
+    myLeads.push(inputEl.value) 
+    render(myLeads)
+    inputEl.value = ""
+    localStorage.setItem("Leads", JSON.stringify(myLeads))
+})
